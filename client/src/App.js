e178853eabc44cdef77c5b7e43d65e41b99e7b78
shinhonggyu <zowoz0@naver.com>
2020-11-30 22:11:01 +0900

    Landing, Navbar

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+
+const App = () => (
+  <>
+    <Navbar />
+    <Landing />
+  </>
+);
+
+export default App;
