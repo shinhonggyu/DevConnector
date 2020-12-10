a333a3406fcc5259f5f055ce4cf0d80f32c31795
shinhonggyu <zowoz0@naver.com>
2020-12-11 02:58:57 +0900

    Profile Experience & Education Display

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
 import { getProfileById } from '../../actions/profileActions';
 import { useParams, Link } from 'react-router-dom';
 
@@ -33,6 +35,36 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth }) => {
           <div className="profile-grid my-1">
             <ProfileTop profile={profile} />
             <ProfileAbout profile={profile} />
+            <div className="profile-exp bg-white p-2">
+              <h2 className="text-primary">Experience</h2>
+              {profile.experience.length > 0 ? (
+                <>
+                  {profile.experience.map((experience) => (
+                    <ProfileExperience
+                      key={experience._id}
+                      experience={experience}
+                    />
+                  ))}
+                </>
+              ) : (
+                <h4>No experience credential</h4>
+              )}
+            </div>
+            <div className="profile-edu bg-white p-2">
+              <h2 className="text-primary">Education</h2>
+              {profile.education.length > 0 ? (
+                <>
+                  {profile.education.map((education) => (
+                    <ProfileEducation
+                      key={education._id}
+                      education={education}
+                    />
+                  ))}
+                </>
+              ) : (
+                <h4>No education credential</h4>
+              )}
+            </div>
           </div>
         </>
       )}
